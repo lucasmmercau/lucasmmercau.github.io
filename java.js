71659864e1adae90a8613470c1a173a82eba323f
lucasmmercau <102770244+lucasmmercau@users.noreply.github.com>
2022-06-08 20:13:03 -0300

    carrusel y formu

--- java.js
+++ java.js
@@ -1,22 +1,35 @@
+console.log("scrip cargada")
+
 var imagen = [];
-imagen[0] = "imagenes/1.jpeg"
-imagen[1] = "imagenes/2.jpeg"
-imagen[2] = "imagenes/3.png"
-imagen[3] = "imagenes/4.jpg"
- var i=0;
+imagen[0] = "./imagenes/1.jpeg"
+imagen[1] = "./imagenes/2.jpeg"
+imagen[2] = "./imagenes/3.png"
+imagen[3] = "./imagenes/4.jpg"
+var i = 0;
+var carruimg = document.getElementById("carru")
 
-function Derecha(){
-    i--
-    if(i == 0){
-        i = 5;
+function Izquierda() {
+    console.log("scrip cargada", imagen)
+    if (i > 0) {
+        i--;
+        carruimg.src = imagen[i];
+
+    } else {
+
+        i = imagen.length - 1;
+        carruimg.src = imagen[i];
     }
-    document.getElementById("carru").src = img[i];
+
 }
 
-function Derecha(){
-    i++
-    if(i == 6){
-        i = 0;
+function Derecha() {
+
+    if (i < imagen.length - 1) {
+        i++;
+        carruimg.src = imagen[i];
+    } else {
+        carruimg.src = imagen[0]
+        i = 1;
     }
-    document.getElementById("carru").src = img[i];
 }
+//formulario
